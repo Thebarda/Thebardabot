b48daf58971e133815e0c45cb9705be544fc36eb
Thebarda <tomdar87@outlook.com>
2023-01-21 10:11:16 +0100

    Fix UI

--- src/Input/ContentEditable.tsx
+++ src/Input/ContentEditable.tsx
@@ -1,19 +1,25 @@
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { Typography } from "@mui/material";
 import { useCallback, useEffect, useLayoutEffect, useState } from "react";
 import { makeStyles } from "tss-react/mui";
 
 const useStyles = makeStyles()((theme) => ({
   input: {
     backgroundColor: theme.palette.grey[800],
-    padding: theme.spacing(0.5, 1),
-    "& > p": {
+    "& p": {
       margin: 0,
     },
+  },
+  placeholder: {
+    color: theme.palette.grey[500],
+  },
+  contentEditable: {
+    marginTop: "-22px",
   },
 }));
 
 const ContentEditable = (): JSX.Element => {
-  const { classes } = useStyles();
+  const { classes, cx } = useStyles();
   const [editor] = useLexicalComposerContext();
   const [isEditable, setEditable] = useState(false);
   const [root, setRoot] = useState("");
@@ -40,8 +46,14 @@ const ContentEditable = (): JSX.Element => {
 
   return (
     <div className={classes.input}>
-      {root.length === 0 && <p>placeholder</p>}
-      <div contentEditable={isEditable} ref={ref} />
+      {root.length === 0 && (
+        <Typography className={classes.placeholder}>Send a message</Typography>
+      )}
+      <div
+        contentEditable={isEditable}
+        ref={ref}
+        className={cx(root.length === 0 && classes.contentEditable)}
+      />
     </div>
   );
 };
